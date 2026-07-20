4003c73abfab8df53b8c0e5db891015a591dad7b
Shagufta <sshagufta63@gmail.com>
2026-07-20 17:12:45 +0530

    Add files via upload
    day2

---
+++ AI52/Week01/Day2/challenge1.py
@@ -0,0 +1,27 @@
+#Problem
+
+#Write a program that asks:
+
+# Name:
+# Years of Experience:
+# Favourite Technology:
+
+# Output:
+
+# Hello Shagufta!
+
+# You have 10 years of experience.
+
+# Looks like you're an expert in BigQuery.
+
+print("Please enter your name: ")
+name =input()
+print("Please enter your years of experience: ")
+year_of_experience=input()
+print("Please enter your favourite technology: ")
+
+fav_tech= input()
+
+print("Hello ", name)
+print("You have ", year_of_experience , "years of experience")
+print("Looks like you are an expert in ", fav_tech)
