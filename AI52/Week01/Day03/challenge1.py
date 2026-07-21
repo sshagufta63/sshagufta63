3cede59845e8c54d7679f603b4907b93d742f6e9
Shagufta Kazi <sshagufta63@gmail.com>
2026-07-21 17:47:50 +0530

    Day 3

---
+++ AI52/Week01/Day03/challenge1.py
@@ -0,0 +1,21 @@
+# Challenge 1 ⭐
+
+# Your mission:
+
+# Write a Python program that:
+
+# Connects to your local Ollama.
+# Uses the Gemma model (or whichever model you downloaded).
+# Sends this prompt:
+# Explain Kubernetes in one sentence.
+# Prints the response.
+
+
+from ollama import chat
+from ollama import ChatResponse
+
+query = "Explain Kubernetes in one sentence."
+messagePayload = {'role':'user', 'content':query}
+
+response: ChatResponse = chat(model='gemma4:12b', messages=[messagePayload])
+print(response)
